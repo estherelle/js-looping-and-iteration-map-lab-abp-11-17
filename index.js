905f7e455821d502205fff358994005b4c91c771
Esther Liao <erliao@princeton.edu>
2017-11-10 18:19:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  return drivers.map(function(value){ return value.toLowerCase(); });
+}
+
+function nameToAttributes(drivers) {
+  const names = drivers.map(function (value) {
+    value = value.split(' ');
+    console.log(value);
+    return Object.assign({}, {firstName: value[0], lastName: value[1]});
+    // let obj = Object.assign({}, { firstName: names[0], lastName: names[1] });
+    // console.log(obj);
+
+  });
+  console.log(names);
+  return names;
+}
+
+function attributesToPhrase(drivers) {
+  const locs = drivers.map(function(value) {
+    console.log(value);
+    console.log(`${value.name} is from ${value.hometown}`);
+    return `${value.name} is from ${value.hometown}`;
+  })
+  return locs;
+}
